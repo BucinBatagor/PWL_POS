f77d9cdeb016e3254bff3e1d2c4255ae5eeff95d
Nizar Khawarizmi <nizar.banme@gmail.com>
2024-03-12 11:26:29 +0700

    Praktikum-3

---
+++ database/seeders/KategoriSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class KategoriSeeder extends Seeder
+{
+    public function run(): void
+    {
+       $data = [
+            [
+                'kategori_id' => 1,
+                'kategori_kode' => 'KAT001',
+                'kategori_nama' => 'Elektronik',
+            ],
+            [
+                'kategori_id' => 2,
+                'kategori_kode' => 'KAT002',
+                'kategori_nama' => 'Pakaian',
+            ],
+            [
+                'kategori_id' => 3,
+                'kategori_kode' => 'KAT003',
+                'kategori_nama' => 'Perabotan Rumah Tangga',
+            ],
+            [
+                'kategori_id' => 4,
+                'kategori_kode' => 'KAT004',
+                'kategori_nama' => 'Otomotif',
+            ],
+            [
+                'kategori_id' => 5,
+                'kategori_kode' => 'KAT005',
+                'kategori_nama' => 'Makanan',
+            ]
+        ];
+        DB::table('m_kategori')->insert($data);
+    }
+}
