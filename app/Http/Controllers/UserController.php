d847224caacba59a4b75ac86b4bf54a403eb8ba4
Nizar Khawarizmi <nizar.banme@gmail.com>
2024-03-22 07:19:50 +0700

    Praktikum-2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,16 +10,21 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
-        $user -> save();
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+
+        $user->username = 'manager12';
         
-        return view('user', ['data' => $user]);
+        $user->save();
+
+        $user->wasChanged(); // True
+        $user->wasChanged('username'); // True
+        $user->wasChanged(['username', 'level_id']); // True
+        $user->wasChanged('nama'); // False
+        $user->wasChanged(['nama', 'username']); // True
     }
 }
