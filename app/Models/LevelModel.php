34468766e8b26aa4af62400ed300da8910da20e5
Nizar Khawarizmi <nizar.banme@gmail.com>
2024-04-08 22:27:57 +0700

    E. Tugas

--- app/Models/LevelModel.php
+++ app/Models/LevelModel.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class LevelModel extends Model
 {
     use HasFactory;
     protected $table ='m_level';
     protected $primaryKey = 'level_id';
+
+    public function user():HasMany{
+        return $this ->hasMany(m_user::class, 'level_id', 'level_id');
+    }
 }
