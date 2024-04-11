5db48d8991c9396423fb56870917b1cbae74e1a5
Nizar Khawarizmi <nizar.banme@gmail.com>
2024-04-11 20:50:40 +0700

    Praktikum 3 – Implementasi jQuery Datatable di AdminLTE :

--- routes/web.php
+++ routes/web.php
@@ -33,5 +33,15 @@ Route::put('/kategori/update/{id}',[KategoriController::class, 'update'])->name(
 Route::get('/kategori/delete/{id}',[KategoriController::class, 'delete'])->name('kategori.delete');
 
 Route::resource('m_user', POSController::class);
-
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);          // Menampilkan data user
+    Route::post('/list', [UserController::class, 'list']);      // menampilkan data user dalam bentukjson untuk datatables
+    Route::get('/create', [UserController::class, 'create']);   // menampilkan form tambah user
+    Route::post('/', [UserController::class, 'store']);         // menyimpan data user
+    Route::get('/{id}', [UserController::class, 'show']);       // menampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);  // menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);     // menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+});
